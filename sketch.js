3183e006bf6c92c6bc1bc713b50acb3638c6baf7
Ritvik-Arch <70333225+Ritvik-Arch@users.noreply.github.com>
2021-01-28 20:18:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 var m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13;
+var score=0;
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+var timer=60;
 var doctor, doctor_img;
+var vaccine, vacine_img;
+var corona, corona_img;
 
 function preload(){
   doctor_img=loadImage("D.png");
+  vaccine_img=loadImage("V.png");
+  corona_img=loadImage("Cv (1).png");
 }
 
 function setup() {
@@ -12,6 +21,16 @@ function setup() {
 doctor=createSprite(40,60,30,30);
 doctor.addImage(doctor_img);
 doctor.scale=0.4;
+
+//Vaccine and Virus
+vaccine=createSprite(1000,500,10,10);
+vaccine.addImage(vaccine_img);
+//vaccine.scale=0.7;
+
+corona=createSprite(600,10,30,30);
+corona.addImage(corona_img);
+corona.scale=0.2;
+
   
 //Create Maze
 m1= createSprite(180,25,10,100);
@@ -20,8 +39,6 @@ m3= createSprite(60,140,100,10);
 m4= createSprite(110,185,10,100);
 
 }
-
-
 
 function draw() {
   background(247,222,96); 
